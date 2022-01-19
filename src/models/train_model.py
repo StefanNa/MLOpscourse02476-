8395bcdb8278f05ff909bf7630ee72176e913a00
slangen <steven_steven@hotmail.de>
2022-01-19 14:30:26 +0100

    finished

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,16 +1,35 @@
 import argparse
+import os
+import pickle
 import sys
 
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
-from torch import optim, nn
+from sklearn.model_selection import train_test_split
+from torch import nn, optim
 from torch.utils.data import DataLoader, Subset
 
 from dataset_fetcher import Dataset_fetcher as DSF
 from model import MyAwesomeModel
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from google.cloud import storage
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+    print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
 def saveplot(loss,PATH='../../reports/figures/'):
     plt.plot([i for i in range(len(loss))],loss)
@@ -76,12 +95,12 @@ class TrainOREvaluate(object):
         print(args)
 
 
-        checkpoint='models/processed/corruptmnist/'#args.c
+        checkpoint='models/dict/corruptmnist/'#args.c
+        picklepath='models/pickle/corruptmnist/'
         checkpoint_name='checkpoint.pth'#args.cname
         lr=args.lr
         epochs=args.e
         batchsize=64#args.b
-        continue_training=None#args.cont
         PATH_IMG='data/processed/corruptmnist/train_images.pt'#args.PATH_IMG
         PATH_LAB='data/processed/corruptmnist/train_labels.pt'#args.PATH_LAB
 
@@ -90,7 +109,8 @@ class TrainOREvaluate(object):
 
         if not os.path.exists(checkpoint):
             os.makedirs(checkpoint)
-        
+        if not os.path.exists(picklepath):
+            os.makedirs(picklepath)
         #get dataset
         dataset=DSF(PATH_IMG,PATH_LAB)
 
@@ -113,19 +133,11 @@ class TrainOREvaluate(object):
    
         model = MyAwesomeModel()
 
-        if continue_training is None:
-            True
-        else:
-            if not os.path.isfile(continue_training):
-                raise ImportError('model dict file is not present')
-            state_dict = torch.load(continue_training)
-            model.load_state_dict(state_dict)
-
         criterion = nn.NLLLoss()
         optimizer = optim.Adam(model.parameters(), lr=lr)
 
         train_losses=[]
-
+        best_accuracy=0
         for e in range(epochs):
             running_loss=0
             model.train()
@@ -139,11 +151,10 @@ class TrainOREvaluate(object):
                 
                 running_loss += loss.item()
             train_losses.append(running_loss)
-            model.eval()
             # print(running_loss)
             if e%3==0:
-                torch.save(model.state_dict(), checkpoint+checkpoint_name)
-                # torch.save(model.state_dict(), 'checkpoint'+str(e)+'.pth')
+                model.eval()
+                
                 with torch.no_grad():
                 # set model to evaluation mode
                     model.eval()
@@ -156,9 +167,23 @@ class TrainOREvaluate(object):
                     accuracy=correct.type(torch.FloatTensor).mean()
                     accuracies.append(accuracy.item()*100)
                 print(f'Accuracy: {np.mean(accuracies)}%')
+                if best_accuracy<np.mean(accuracies):
+                    best_accuracy=np.mean(accuracies)
+                    torch.save(model,picklepath+'model.pth')
+                    torch.save(model.state_dict(), checkpoint+checkpoint_name)
+
         if not os.path.isdir('reports/figures/'):
             os.makedirs('reports/figures/')
         saveplot(train_losses,PATH='reports/figures/')
+
+        bucket_name="mlops-project-6"
+        source_file_checkpoint=checkpoint+"checkpoint.pth"
+        source_file_pickle=picklepath+"model.pth"
+        destination_blob_name="mnist/models/"
+
+        upload_blob(bucket_name, source_file_pickle, destination_blob_name+'pickle/model.pth')
+        upload_blob(bucket_name, source_file_checkpoint, destination_blob_name+'dict/checkpoint.pth')
+
         
     def evaluate(self):
         # print("Evaluating until hitting the ceiling")
